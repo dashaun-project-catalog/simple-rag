474aa07ded62612af6c22169c1c1fa874207edc8
DaShaun <dashaun@dashaun.com>
2024-04-23 20:00:11 -0500

    tests compose

--- src/test/java/dev/dashaun/ai/ApplicationTests.java
+++ src/test/java/dev/dashaun/ai/ApplicationTests.java
@@ -2,9 +2,25 @@ package dev.dashaun.ai;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.ollama.OllamaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 class ApplicationTests {
+
+    @Bean
+    OllamaContainer ollamaContainer() throws Exception {
+        OllamaContainer instance = new OllamaContainer(
+                DockerImageName.parse("dashaun/ollama-mistral:7B")
+                        .asCompatibleSubstituteFor("ollama/ollama")
+        );
+        instance.start();
+        instance.execInContainer("ollama", "pull", "mistral");
+        System.setProperty("OLLAMA_HOST", instance.getHost());
+        System.setProperty("OLLAMA_PORT", instance.getMappedPort(11434).toString());
+        return instance;
+    }
 
     @Test
     void contextLoads() {
--- src/test/java/dev/dashaun/ai/TestApplication.java
+++ src/test/java/dev/dashaun/ai/TestApplication.java
@@ -2,7 +2,6 @@ package dev.dashaun.ai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.ollama.OllamaContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -12,10 +11,12 @@ public class TestApplication {
 
     @Bean
     OllamaContainer ollamaContainer() throws Exception {
-        OllamaContainer instance = new OllamaContainer(DockerImageName.parse("ollama/ollama:0.1.26")).withExposedPorts(11434);
+        OllamaContainer instance = new OllamaContainer(
+                DockerImageName.parse("dashaun/ollama-mistral:7B")
+                        .asCompatibleSubstituteFor("ollama/ollama")
+        );
         instance.start();
         instance.execInContainer("ollama", "pull", "mistral");
-        instance.commitToImage("dashaun/ollama-mistral:7B");
         System.setProperty("OLLAMA_HOST", instance.getHost());
         System.setProperty("OLLAMA_PORT", instance.getMappedPort(11434).toString());
         return instance;
